b1a8946581517b3689ce4ad6ca5648fa3d49338d
lichengb <1031658441@qq.com>
2017-10-29 00:08:47 +0800

    FISHINGJOY

--- Classes/StaticData.h
+++ Classes/StaticData.h
@@ -21,6 +21,14 @@ using cocos2d::CCDictionary;
 using cocos2d::CCPointFromString;
 using cocos2d::CCRectFromString;
 using cocos2d::CCSizeFromString;
+#define STATIC_DATA_FILENAME "static_data.plist"
+#define STATIC_DATA_STRING(key) StaticData::sharedStaticData()->stringValueFromKey(key)
+#define STATIC_DATA_INT(key) StaticData::sharedStaticData()->intValueFromKey(key)
+#define STATIC_DATA_FlOAT(key) StaticData::sharedStaticData()->floatValueFromKey(key)
+#define STATIC_DATA_BOOL(key) StaticData::sharedStaticData()->booleanFromKey(key)
+#define STATIC_DATA_POINT(key) StaticData::sharedStaticData()->pointFromKey(key)
+#define STATIC_DATA_RECT(key) StaticData::sharedStaticData()->rectFromKey(key)
+#define STATIC_DATA_SIZE(key) StaticData::sharedStaticData()->sizeFromKey(key)
 
 class StaticData {
 public:
